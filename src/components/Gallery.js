2459c6d108edb5b7a18a1952fa09d73820383265
georgedoescode <georgedoescode@gmail.com>
2019-08-05 19:41:00 +0100

    nav changes

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -7,4 +7,27 @@ export default styled.section`
   @media only screen and (max-width: 720px) {
     padding: 1.5rem 0;
   }
+
+  .header {
+    width: 100%;
+    background: #fff;
+    margin-bottom: 1.5rem;
+    padding: 1rem;
+    /* min-height: 400px; */
+    border: 2px solid #222;
+    padding-bottom: 2rem;
+    width: 66.66666666666666%;
+
+    h1 {
+      font-size: 2.25rem;
+      margin-bottom: 1.5rem;
+      margin-top: 0.5rem;
+      line-height: 1;
+    }
+
+    p {
+      font-size: 1rem;
+      /* max-width: 75%; */
+    }
+  }
 `
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,30 +3,32 @@ import styled from "styled-components"
 
 const Nav = styled.nav`
   width: 100%;
-  height: 6rem;
+  /* height: 5.5rem; */
   display: flex;
   align-items: center;
-  background: #fff;
-  padding: 0 25.5px;
+  /* background: #fff; */
+  padding: 2rem 25.5px 0.5rem;
+  /* border-bottom: 2px solid #222; */
 
   @media only screen and (max-width: 720px) {
-    height: 5rem;
-    padding: 0 1rem;
+    /* padding: 0 1rem; */
+    padding: 1.5rem 1rem 0rem;
   }
 
   p {
     line-height: 1;
     font-weight: 400;
     font-family: "Space Mono";
-    font-size: 1.125rem;
-    background: #111;
-    color: #fff;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    padding: 0.5rem 0.75rem;
+    font-size: 1rem;
+    background: #fff;
+    color: #111;
+    /* -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale; */
+    padding: 0.625rem 0.75rem;
+    border: 2px solid #111;
 
     @media only screen and (max-width: 720px) {
-      font-size: 1rem;
+      font-size: 0.75rem;
     }
   }
 `
